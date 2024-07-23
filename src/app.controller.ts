75fc6011ce8bf21bf38c38f3c6d58a9ecdfab586
Fabrício José da Silva <fabriciojsilva89@gmail.com>
2024-07-23 19:02:55 -0300

    add assetlink

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -20,5 +20,18 @@ export class AppController {
       res.redirect(dto.link);
     }
   }
+
+  @Get('.well-known/assetlink.json')
+  getAssetLink(@Res() res) {
+    return res.json([{
+      "relation": ["delegate_permission/common.handle_all_urls"],
+      "target": {
+        "namespace": "android_app",
+        "package_name": "com.example.deep_link",
+        "sha256_cert_fingerprints":
+        ["A1:61:C9:09:30:4B:AF:E6:1A:3D:A3:1D:C1:38:9E:B5:5D:EE:50:B7:70:44:0B:E3:24:92:99:27:5E:2B:6C:EE"]
+      }
+    }]);
+  }
 }
 
