b6799bc15a0e5ddb11cba03f6fdd8a17677fb32b
Fabrício José da Silva <fabriciojsilva89@gmail.com>
2024-07-25 14:51:25 -0300

    remove database

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -20,28 +20,14 @@ export class AppController {
   @Get()
   async redirect(@Query() dto: ParamDto, @Req() req, @Res() res): Promise<void> {
     const userAgent = req.headers['user-agent'];
-    const exist = await this.linksRepository.findOneBy({ ip: req.ip, deleted: false });
-    if (exist){
-      this.linksRepository.save({ link: exist.link, ip: exist.ip, deleted: true });
-    }
-
 
     if (userAgent.includes('iPhone') || userAgent.includes('iPad')) {
-      this.linksRepository.save({ link: dto.link, ip: req.ip, deleted: false });
       res.redirect('https://apps.apple.com/app/id'+dto.ibi);
     } else if (userAgent.includes('Android')) {
-      this.linksRepository.save({ link: dto.link, ip: req.ip, deleted: false });
       res.redirect('https://play.google.com/store/apps/details?id='+dto.apn+'&pcampaignid=fdl_long&url='+dto.link);
     } else {
       res.redirect(dto.link);
     }
-  }
-
-  @Get('link')
-  async link(@Query() ip: string): Promise<LinksEntity> {
-    const link = await this.linksRepository.findOneBy({ ip: ip });
-    this.linksRepository.save({ link: link.link, ip: link.ip, deleted: true });
-    return link;
   }
 }
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { LinksEntity } from './entities/links.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'src/database/eduk-dynamic-link.db',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
-    TypeOrmModule.forFeature([LinksEntity])
-  ],
+  // imports: [
+  //   TypeOrmModule.forRoot({
+  //     type: 'sqlite',
+  //     database: 'src/database/eduk-dynamic-link.db',
+  //     entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  //     synchronize: true,
+  //   }),
+  //   TypeOrmModule.forFeature([LinksEntity])
+  // ],
   controllers: [AppController],
 })
 
