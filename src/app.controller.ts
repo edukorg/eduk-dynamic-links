691ca8944b7544fc85a9630d96628ac86a6eba42
Fabrício José da Silva <fabriciojsilva89@gmail.com>
2024-07-26 16:54:40 -0300

    created persistence link rule

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,7 @@
 import { Controller, Get, Query, Req, Res } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { LinksEntity } from './entities/links.entity';
 
 class ParamDto {
   link: string;
@@ -9,17 +12,36 @@ class ParamDto {
 
 @Controller()
 export class AppController {
+  constructor(
+    @InjectRepository(LinksEntity) 
+    private linksRepository: Repository<LinksEntity>
+  ) {}
+
   @Get()
   async redirect(@Query() dto: ParamDto, @Req() req, @Res() res): Promise<void> {
     const userAgent = req.headers['user-agent'];
+    const exist = await this.linksRepository.findOneBy({ ip: req.ip, deleted: false });
+    if (exist) {
+      await this.linksRepository.update({ ip: req.ip }, { deleted: true });
+    }
 
-    if (userAgent.includes('iPhone') || userAgent.includes('iPad')) {
-      res.redirect('https://apps.apple.com/app/id'+dto.isi);
-    } else if (userAgent.includes('Android')) {
-      res.redirect('https://play.google.com/store/apps/details?id='+dto.apn+'&pcampaignid=fdl_long&url='+dto.link);
+    if (userAgent.includes('iPhone') || userAgent.includes('iPad') || userAgent.includes('Android')) {
+      await this.linksRepository.save({ ip: req.ip, link: dto.link, deleted: false });
+
+      if (userAgent.includes('iPhone') || userAgent.includes('iPad')) {
+        res.redirect('https://apps.apple.com/app/id'+dto.isi);
+      } else {
+        res.redirect('https://play.google.com/store/apps/details?id='+dto.apn+'&pcampaignid=fdl_long&url='+dto.link);
+      }
     } else {
       res.redirect(dto.link);
     }
   }
+
+  @Get('link')
+  async getLink(@Req() req): Promise<LinksEntity> {
+    return await this.linksRepository.findOneBy({ ip: req.ip, deleted: false });
+  }
+
 }
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { LinksEntity } from './entities/links.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'edul-dynamic-links.db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([LinksEntity]),
+  ],
   controllers: [AppController],
 })
 
